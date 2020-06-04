5c28191a5ffdaf5ce7ea515e1207b904da9a5476
Drkeck <k3ckl3y.a@gmail.com>
2020-06-04 05:52:22 -0700

    added CRUD for posts made by users. and made new posts show up first in the GET ALL call

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -43,11 +43,11 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 // LOGIN /api/users/login
